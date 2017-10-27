351c99fbcc77ebbf05b611926afd7e4b2e5c0596
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2017-10-27 02:02:39 -0400

    add reward tier model

--- app/models/reward_tier.rb
+++ app/models/reward_tier.rb
@@ -1,2 +1,4 @@
 class RewardTier < ApplicationRecord
+	belongs_to :project
+	has_many :users
 end
--- db/migrate/20171027055302_create_reward_tiers.rb
+++ db/migrate/20171027055302_create_reward_tiers.rb
@@ -1,7 +1,10 @@
 class CreateRewardTiers < ActiveRecord::Migration[5.1]
   def change
     create_table :reward_tiers do |t|
-
+      t.string :pledge_requirement 
+      t.string :name 
+      t.text :description 
+      t.integer :backer_amount
       t.timestamps
     end
   end
