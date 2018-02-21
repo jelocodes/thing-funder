7c10d36d1b5b974859fbe565769641411852a89d
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-02-21 18:31:05 -0500

    add comment deletion functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 
 	before_action :find_commentable
+	before_action :set_comment, only: [:show, :edit, :update, :destroy]
+	before_action :authorize_user, only: [:destroy]	
 
 	def new 
 		@comment = Comment.new 
@@ -17,28 +19,33 @@ class CommentsController < ApplicationController
 	end
 
 	def update 
-		@comment = Comment.find(params[:id])
-		# authorize @comment
 		@comment.update(comment_params)
 		redirect_to :back, notice: 'Your comment was edited successfully!'
 	end
 
-	def delete 
-		@comment = Comment.find(params[:id])
-		# authorize @comment 
+	def destroy
+		@project = @comment.commentable
 		@comment.destroy
-		redirect_to :back, notice: 'Your comment was deleted!'
+		redirect_to project_path(@project), notice: 'Your comment was deleted!'
 	end
 
 	private 
+
+	def set_comment
+		@comment = Comment.find(params[:id])
+	end
+
+    def find_commentable
+      @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+      @commentable = Project.find_by_id(params[:project_id]) if params[:id] || params[:comment][:project_id] 
+    end
 
 	def comment_params 
 		params.require(:comment).permit(:content, :user_id, :commentable_id)
 	end
 
-    def find_commentable
-      @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-      @commentable = Project.find_by_id(params[:project_id]) if params[:comment][:project_id]
-    end
+	def authorize_user
+		redirect_to '/', alert: "You're not allowed to do that!" unless current_user && current_user.admin? || current_user.try(:id) == @comment.user_id
+	end
 
 end
