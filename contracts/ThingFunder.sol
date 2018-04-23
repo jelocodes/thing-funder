69cd27872a6483400dd6b50492260544959ac255
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-04-23 13:26:12 -0400

    add divisor logic

--- contracts/ThingFunder.sol
+++ contracts/ThingFunder.sol
@@ -1,10 +1,20 @@
 contract ThingFunder {
 	address public backer;
 	address public maker; 
+	uint divisor;
 
 	constructor (address _maker) {
 		backer = msg.sender;
 		maker = _maker;
+		divisor = 0;
+	}
+
+	function setDivisor() {
+		divisor = address(this).balance/uint(4);
+	}
+
+	function getDivisor() constant returns(uint) {
+		return divisor;
 	}
 
 	function payoutToMaker() payable {
