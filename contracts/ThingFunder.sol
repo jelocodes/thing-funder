8ec3941adf5df9b6cbac39c6894f6af200c76733
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-04-24 14:14:19 -0400

    allow giving back to backers only if maker has been paid out

--- contracts/ThingFunder.sol
+++ contracts/ThingFunder.sol
@@ -5,6 +5,7 @@ contract ThingFunder {
 	address public maker; 
 	uint divisor;
 	uint timesRefunded;
+	uint timesBacked;
 
 	constructor (address _maker) payable {
 		backer = msg.sender;
@@ -12,6 +13,7 @@ contract ThingFunder {
 		divisor = address(this).balance/uint(4);
 		maker.transfer(divisor);
 		timesRefunded = 0;
+		timesBacked = 1;
 	}
 
 	function getDivisor() constant returns(uint) {
@@ -21,13 +23,15 @@ contract ThingFunder {
 	function payoutToMaker() payable {
 		if (msg.sender == backer) {
 			maker.transfer(divisor);
+			timesBacked += 1;
 		}
 	}
 	
 	function refundToBacker() payable {
-	    require(msg.sender == maker && msg.value == divisor);
+	    require(msg.sender == maker && msg.value == divisor && timesBacked > 0);
 	    backer.transfer(divisor);
 	    timesRefunded += 1;
+	    timesBacked -= 1;
 	}
 
 	function fundAgain() payable {
@@ -42,7 +46,12 @@ contract ThingFunder {
 	function getTimesRefunded() constant returns (uint) {
 		return timesRefunded;
 	}
+	
+	function getTimesBacked() constant returns (uint) {
+	    return timesBacked;
+	}
 }
 
-// allow for repeated deposits if they have been refunded and time has yet to run out
+// allow for repeated deposits if they have been refunded and time has yet to run out[check]
 // add time/date run out... everything works except the check to keep funding even if timesRefunded is less than 0
+// allow only giving back to donors if they have been paid out, add a pay out counter [check]
