32a68118f86f2dbf21ad48b38f6f925110b9a000
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-04-28 19:10:39 -0400

    add getter and setter methods and factory functionality

--- contracts/ThingFactory.sol
+++ contracts/ThingFactory.sol
@@ -2,5 +2,20 @@ pragma solidity ^0.4.12;
 import "./ThingFunder.sol";
 
 contract ThingFactory {
-
+    // index of created contracts
+    address[] public projects;
+    
+    function getProjectCount() public constant returns(uint projectCount) {
+        return projects.length;
+    }
+    
+    function getLastProject(uint _index) public constant returns(address lastProject) {
+        return projects[_index];
+    }
+    
+    // deploy a new contract 
+    function newProject(address _maker) public {
+        projects.push(new ThingFunder(_maker));
+        return address(projects[projects.length + 1]);
+    }
 }
