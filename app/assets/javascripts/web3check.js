d7f806dbd3cd23597204b531b09abed5b90d9f20
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-04-28 22:28:08 -0400

    add code to instantiate web3

--- app/assets/javascripts/web3check.js
+++ app/assets/javascripts/web3check.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
 	if (typeof web3 !== 'undefined') {
-		// startApp(web3);
+		startApp(web3);
 		console.log('loaded!');
 	} else {
 		// Warn the user that they need to get a web3 browser
@@ -8,3 +8,13 @@ window.addEventListener('load', function() {
 		console.log('Download MetaMask');
 	}
 })
+
+const Eth = require('ethjs-query');
+const EthContract = require('ethjs-contract');
+
+function startApp(web3) {
+	const eth = new Eth(web3.currentProvider);
+	const contract = new EthContract(eth);
+
+	initContract(contract);
+}
