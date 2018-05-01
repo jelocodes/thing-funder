eeb59504cf512e8b74325278629411a0152cfea7
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-05-01 01:04:07 -0400

    add new helper to check if project has been funded

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,4 +4,9 @@ module ProjectsHelper
 		(project.deadline - Date.today).to_i
 	end
 
+	def funded?(project)
+		project.update(funds: 0) if project.funds == nil 
+		project.funds / project.goal == 100
+	end
+
 end
