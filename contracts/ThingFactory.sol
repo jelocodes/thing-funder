06c2c48a13def279876333111b945e5ee800547b
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-05-05 17:37:53 -0400

    add store and getter functions

--- contracts/ThingFactory.sol
+++ contracts/ThingFactory.sol
@@ -3,8 +3,18 @@ import "./ThingFunder.sol";
 
 contract ThingFactory {
     
+    address[] public contracts;
+    
+    event newContractOutput(address newContract);
+    
     // deploy a new contract 
     function newProject(address _maker) public returns(address newContract) {
-        return new ThingFunder(_maker);
+        ThingFunder t = new ThingFunder(_maker);
+        contracts.push(t);
+        return t; 
+    }
+    
+    function lastContractCreated() public returns(address lastContract) {
+        return contracts[contracts.length -1];
     }
 }
