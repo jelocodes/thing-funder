14a8337b5567a5645c449817533ea4d0d75ac183
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-05-06 19:33:11 -0400

    change thing funder contract to work without factory contract

--- contracts/ThingFunder.sol
+++ contracts/ThingFunder.sol
@@ -7,18 +7,13 @@ contract ThingFunder {
 	uint timesRefunded;
 	uint timesBacked;
 
-	constructor (address _maker) {
+	constructor (address _maker) payable {
 		backer = msg.sender;
 		maker = _maker;
-		timesBacked = 0;
-	}
-	
-	function initialize() payable {
-	    require(timesBacked == 0);
 		divisor = address(this).balance/uint(4);
 		maker.transfer(divisor);
 		timesRefunded = 0;
-		timesBacked = 1;	    
+		timesBacked = 1;
 	}
 
 	function getDivisor() constant returns(uint) {
