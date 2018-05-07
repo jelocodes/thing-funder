07ea26b4a9f56007a61c21ce2be28954e0936763
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-05-07 02:07:29 -0400

    add reward and user relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501042508) do
+ActiveRecord::Schema.define(version: 20180505222253) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20180501042508) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "contract"
     t.index ["project_id"], name: "index_project_backers_on_project_id"
     t.index ["user_id"], name: "index_project_backers_on_user_id"
   end
