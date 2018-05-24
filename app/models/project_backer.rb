f617629b702bc281d9c8f0df2c70fa0df371ddc8
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-05-24 19:25:45 -0400

    get right-most column of each project show page to load asynchronously

--- app/models/project_backer.rb
+++ app/models/project_backer.rb
@@ -1,4 +1,8 @@
 class ProjectBacker < ApplicationRecord
 	belongs_to :project 
 	belongs_to :user
+
+	def percentage_backed 
+		return self.times_backed == nil ? 25 : (((self.times_backed).to_f / 4.to_f) * 100).to_i 
+	end
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,4 +3,8 @@ class Reward < ApplicationRecord
 	has_many :user_rewards, dependent: :destroy
 	has_many :users, through: :user_rewards
 	validates_presence_of :name, :description, :pledge_requirement
+
+	def number_claimed
+		return self.users.length
+	end
 end
